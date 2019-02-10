f8f57d12d69514723c6ecdb825ec79b24031b1bc
Nikita Tret <accauntnikity@gmail.com>
2019-02-11 01:16:36 +0300

    Implementation

--- Population.cs
+++ Population.cs
@@ -6,8 +6,19 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            double tempPopulation = initialPopulation;
+
+            int currentYear = 0;
+
+            do
+            {
+                tempPopulation = tempPopulation + (tempPopulation * (percent / 100)) + visitors;
+                currentYear++;
+            }
+            while (tempPopulation < currentPopulation);
+
+            return currentYear;
         }
     }
 }
+    
